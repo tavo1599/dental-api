ef12aba14834118e72c152c828f0e0af34d19b46
tavo1599 <gustavo08_99@hotmail.com>
2025-12-30 19:10:09 -0500

    tenant module

--- src/tenants/tenants.module.ts
+++ src/tenants/tenants.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Tenant } from './entities/tenant.entity';
-import { TenantsController } from './tenants.controller';
 import { TenantsService } from './tenants.service';
+// IMPORTANTE: Importamos AMBOS controladores
+import { TenantsController, PublicTenantsController } from './tenants.controller';
+import { Tenant } from './entities/tenant.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Tenant])],
-  controllers: [TenantsController],
+  // IMPORTANTE: Agregamos PublicTenantsController al array de controllers
+  controllers: [TenantsController, PublicTenantsController],
   providers: [TenantsService],
+  exports: [TenantsService], // Exportamos por si otros módulos lo necesitan
 })
 export class TenantsModule {}
